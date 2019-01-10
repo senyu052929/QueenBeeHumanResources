90cdc18ad86866337f3499f04d7cad3633214708
sen <senyu0529@aliyun.com>
2019-01-10 11:15:52 +0800

    新建springcloud工程 新建shiro权限模块

---
+++ QueenBee-parent/QueenBee-shiro/src/main/java/org/QueenBee/shiro/App.java
@@ -0,0 +1,13 @@
+package org.QueenBee.shiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
