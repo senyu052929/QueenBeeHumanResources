bb70a896882a9df3a2a81d75a882ea4fa99065bc
sen <liangyi@baosheng-air.com>
2019-01-10 15:21:52 +0800

    完善shiro集成

---
+++ QueenBee-parent/QueenBee-shiro/readme.md
@@ -0,0 +1,4 @@
+权限管理集成shiro
+session使用redis存放
+访问url使用restful,后期移动端，web端都可以使用。可能存在定义不明确
+前端使用springcloud负载均衡访问
---
+++ QueenBee-parent/QueenBee-shiro/src/main/java/org/QueenBee/shiro/dao/UserInfoDao.java
@@ -0,0 +1,9 @@
+package org.QueenBee.shiro.dao;
+
+import org.QueenBee.shiro.entity.UserInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
+    /**通过username查找用户信息;*/
+    public UserInfo findByUsername(String username);
+}
