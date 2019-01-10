e1a2af8b9e2cb7252bd523dbec94f7b5ea4a387c
sen <liangyi@baosheng-air.com>
2019-01-10 19:46:24 +0800

    删除旧文件

--- QueenBee-parent/QueenBee-shiro/src/main/java/org/QueenBee/shiro/service/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package org.QueenBee.shiro.service;
-
-import org.QueenBee.shiro.entity.UserInfo;
-
-public interface UserInfoService {
-    /**通过username查找用户信息;*/
-    public UserInfo findByUsername(String username);
-}
